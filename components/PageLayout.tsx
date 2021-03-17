414a7510ea8ee61287b82df9476e9cb9b72f9010
Slava Bezgachev <bezgachev@gmail.com>
2021-03-17 18:24:07 +0600

    Things

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-const PageLayout: React.FC = ({children}) => {
-
-  return <div className="flex flex-col">
-    <header>
-      Header
-    </header>
-    <main>
-      {children}
-    </main>
-    <footer>
-      Footer
-    </footer>
-  </div>
-
+const PageLayout: React.FC = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="flex">
+        <div className="">
+          <span className="text-4xl font-serif font-semibold">BE dice</span>
+        </div>
+        <div className="flex flex-1 flex-row-reverse">
+          <button>Profile</button>
+          <button>Exit</button>
+        </div>
+      </header>
+      <main className="flex-grow">{children}</main>
+      <footer className="text-center bg-blue-500 text-gray-100">be-dice.com 2021</footer>
+    </div>
+  );
 };
 
 export default PageLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@ import PageLayout from "../components/PageLayout";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <PageLayout>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <PageLayout>
 
-      </PageLayout>
+      The actual page
 
-    </div>
+
+    </PageLayout>
   )
 }
