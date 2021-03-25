dda55b65c07c3bf932ba97b7202e2f700dc713a1
Slava Bezgachev <bezgachev@gmail.com>
2021-03-25 18:06:30 +0600

    Changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Header = () => {
-
+const Header: React.FC = () => {
+  return (
+    <header className="flex">
+      <div className="p-5 transform -skew-x-6">
+        <span className="text-4xl font-serif font-semibold bg-blue-600 p-2 text-white">BE dice</span>
+      </div>
+      <div className="p-5 transform flex flex-1 flex-row-reverse skew-x-6 space-x-2">
+        <button className="border-4 border-blue-400 m-1 px-2">Profile</button>
+        <button className="border-4 border-blue-600 m-1 px-2">Exit</button>
+      </div>
+    </header>
+  );
 };
 
-export default Header();
+export default Header;
--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,18 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 const PageLayout: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="flex">
-        <div className="">
-          <span className="text-4xl font-serif font-semibold">BE dice</span>
-        </div>
-        <div className="flex flex-1 flex-row-reverse">
-          <button>Profile</button>
-          <button>Exit</button>
-        </div>
-      </header>
+      <Header />
+
       <main className="flex-grow">{children}</main>
+
       <footer className="text-center bg-blue-500 text-gray-100">be-dice.com 2021</footer>
     </div>
   );
